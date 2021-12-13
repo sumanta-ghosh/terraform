5b42c00c200295a5991811cc64edfac6fbef1da8
Sumanta <sumanta.ghosh.mit@gmail.com>
2021-12-13 17:45:43 +0530

    configure aws profile

--- first_ec2.tf
+++ first_ec2.tf
@@ -1,5 +1,10 @@
 provider "aws" {
-  region     = "us-west-2"
-  access_key = ""
-  secret_key = ""
+  region  = "us-east-1"
+  profile = "poc"
+}
+
+resource "aws_instance" "my-first-ec2" {
+  ami           = "ami-0ed9277fb7eb570c9"
+  instance_type = "t2.micro"
+
 }
